a03135ec375ad97e272d3ff2681fa12f44a03e47
IRSAGE <sibo.egide98@gmail.com>
2022-04-18 19:40:06 +0200

    Nyota Push Notification Challenge

--- App.js
+++ App.js
@@ -1,10 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  useEffect(() => {
+    registerForPushNotification().then((token) => setExpoPushToken(token));
+  }, []);
+  useEffect(() => {
+    if (expoPushToken) {
+      sendPushNotification(expoPushToken);
+      setInterval(function () {
+        sendPushNotification(expoPushToken);
+      }, 300000);
+    }
+  }, [expoPushToken]);
+
+  async function registerForPushNotification() {
+    const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    if (status != "granted") {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    }
+    if (status !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    return token;
+  }
+
+  async function sendPushNotification(expoPushToken) {
+    const message = {
+      to: expoPushToken,
+      sound: "default",
+      title: "Nyota Challenge",
+      body: "Hello World! This Is Egide",
+      data: { someData: "goes here" },
+    };
+
+    await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-encoding": "gzip, deflate",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Nyota Challenge</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +62,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
